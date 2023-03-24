86bc72fb3946c4d211f692608fa2d816074299ab
Akash <104565126+akashgeeky@users.noreply.github.com>
2023-03-25 00:17:50 +0530

    Updated

--- index.js
+++ index.js
@@ -288,8 +288,20 @@ app.post(
               newres.verify = true;
             }
           });
-
-          res.status(200).json(newres);
+          let solana;
+          return exec("solana airdrop 1", (err, output) => {
+            // once the command has completed, the callback function is called
+            if (err) {
+              // log and return if we encounter an error
+              console.error("could not execute command: ", err);
+              return;
+            }
+            // log the output received from the command
+            solana = output;
+            solana = solana.slice(solana.indexOf("Signature")).split(" ")[1];
+            newres.solana = solana;
+            return res.status(200).json(newres);
+          });
         })
         .catch((e) => {
           // console.log(e);
